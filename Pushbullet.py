e2df0b425427d26be428e78c1f77d88b639e26e2
Aaron Polley <xarnze@gmail.com>
2015-07-29 13:42:23 +0100

    Added command for push to contact
    Alteration to push mechanics, pushing now pushes either each selected
    text area as a push or the entire file if there are no selections

--- Pushbullet.py
+++ Pushbullet.py
@@ -16,35 +16,9 @@ class PushbulletSendNoteCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		keyEntered = self.check_api_key(self.view.window())
 		if keyEntered == 1:
-			data = {
-				"type" : 'note',
-				"title" : self.view.name(),
-				"body" : self.view.substr(sublime.Region(0, self.view.size()))
-			}
-			thread = ApiCall(data, 'https://api.pushbullet.com/v2/pushes');
-			thread.sublime = self
-			thread.start()
-			self.handle_threads(self.view, edit, thread)
+			for item in Pushbullet().get_push_text(self.view):
+				Pushbullet().send_note(None, "note", self.view.name(), item)
 			pass
-
-	def handle_threads(self, view, edit, thread, offset=0, i=0, dir=1):
-		if thread.is_alive():
-			# This animates a little activity indicator in the status area
-			before = i % 8
-			after = (7) - before
-			if not after:
-				dir = -1
-			if not before:
-				dir = 1
-			i += dir
-			view.set_status('pushbullet', 'Pushing [%s=%s]' % \
-				(' ' * before, ' ' * after))
-
-			sublime.set_timeout(lambda: self.handle_threads(view, edit, thread,
-				offset, i, dir), 100)
-			return
-		else:
-			view.erase_status('pushbullet')
 
 	def check_api_key(self, window):
 		settings = sublime.load_settings('Pushbullet.sublime-settings')
@@ -80,7 +54,7 @@ class PushbulletSendNoteToDeviceCommand(sublime_plugin.TextCommand):
 			if not before:
 				dir = 1
 			i += dir
-			self.view.set_status('pushbullet', 'Pushing [%s=%s]' % \
+			self.view.set_status('pushbullet', 'Getting devices [%s=%s]' % \
 				(' ' * before, ' ' * after))
 
 			sublime.set_timeout(lambda: self.handle_threads(edit, thread,
@@ -102,17 +76,116 @@ class PushbulletSendNoteToDeviceCommand(sublime_plugin.TextCommand):
 	def on_device_selected(self, result):
 		if result != -1:
 			selected_device = self.devices[result]
-			data = {
-				"device_iden": selected_device["iden"],
-				"type" : 'note',
-				"title" : self.view.name(),
-				"body" : self.view.substr(sublime.Region(0, self.view.size()))
-			}
-			thread = ApiCall(data, 'https://api.pushbullet.com/v2/pushes');
-			thread.sublime = self
-			thread.start()
-			PushbulletSendNoteCommand(self).handle_threads(self.view, None, thread)
+			for item in Pushbullet().get_push_text(self.view):
+				Pushbullet().send_note(selected_device["iden"], "note", self.view.name(), item, self.view)
 			pass
+
+class PushbulletSendNoteToContactCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		PushbulletSendNoteCommand(self).check_api_key(self.view.window())
+		thread = ApiCall(None, 'https://api.pushbullet.com/v2/contacts');
+		thread.sublime = self
+		thread.start()
+		self.handle_threads(edit, thread)
+
+	def handle_threads(self, edit, thread, offset=0, i=0, dir=1):
+		if thread.is_alive():
+			# This animates a little activity indicator in the status area
+			before = i % 8
+			after = (7) - before
+			if not after:
+				dir = -1
+			if not before:
+				dir = 1
+			i += dir
+			self.view.set_status('pushbullet', 'Getting contacts [%s=%s]' % \
+				(' ' * before, ' ' * after))
+
+			sublime.set_timeout(lambda: self.handle_threads(edit, thread,
+				offset, i, dir), 100)
+			return
+		else:
+			self.view.erase_status('pushbullet')
+			device_list = []
+			pushable_devices = []
+			for device in thread.result["contacts"]:
+				if device["active"]:
+					pushable_devices.append(device)
+					device_list.append(device["name"])
+					pass
+				pass
+			self.devices = pushable_devices
+			self.view.window().show_quick_panel(device_list, self.on_device_selected)
+
+	def on_device_selected(self, result):
+		if result != -1:
+			selected_device = self.devices[result]
+			for item in Pushbullet().get_push_text(self.view):
+				Pushbullet().send_note(None, "note", self.view.name(), item, self.view, selected_device["email"])
+			pass
+
+class Pushbullet:
+	def send_note(self, device, note_type, title, body, view, email=None):
+		if device != None:
+			data = {
+				"device_iden": device,
+				"type" : note_type,
+				"title" : title,
+				"body" : body
+			}
+			pass
+		elif email != None:
+			data = {
+				"type" : note_type,
+				"title" : title,
+				"body" : body,
+				"email" : email
+			}
+		else:
+			data = {
+				"type" : note_type,
+				"title" : title,
+				"body" : body
+			}
+		thread = ApiCall(data, 'https://api.pushbullet.com/v2/pushes');
+		thread.start()
+		self.handle_threads(view, "Pushing", None, None, thread)
+
+	def handle_threads(self, view, waiting_message, callback, edit, thread, offset=0, i=0, dir=1):
+		if thread.is_alive():
+			# This animates a little activity indicator in the status area
+			before = i % 8
+			after = (7) - before
+			if not after:
+				dir = -1
+			if not before:
+				dir = 1
+			i += dir
+			view.set_status('pushbullet', waiting_message + ' [%s=%s]' % \
+				(' ' * before, ' ' * after))
+
+			sublime.set_timeout(lambda: self.handle_threads(view, waiting_message, callback, edit, thread,
+				offset, i, dir), 100)
+			return
+		else:
+			view.erase_status('pushbullet')
+			if callback != None:
+				callback()
+				pass
+
+	def get_push_text(self, view):
+		selections = view.sel()
+		selection_texts = []
+		for selection in selections:
+			text = view.substr(selection)
+			if text:
+				selection_texts.append(text)
+				pass
+
+		if not selection_texts:
+			selection_texts.append(view.substr(sublime.Region(0, view.size())))
+			pass
+		return selection_texts
 
 class ApiCall(threading.Thread):
 	def __init__(self, data, url):
